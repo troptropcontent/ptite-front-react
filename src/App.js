547f6084f10752890379d6a0a7a9456ca7ec3b44
Tom <tomecrepont@gmail.com>
2021-04-01 15:40:45 +0200

    Intalled React Routers and everything, ready to rock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Dashboard from './components/Dashboard'
+import Preferences from './components/Preferences'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Hello from APP</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard/>
+          </Route>
+          <Route path="/preferences">
+            <Preferences/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Preferences.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Preferences = () => {
+  return (
+  <h2>Preferences</h2>
+  )
+}
+
+export default Preferences
