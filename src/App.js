df4381eb5fe0cc2398779afc0e403e3010193315
Tom <tomecrepont@gmail.com>
2021-04-09 17:59:14 +0200

    Changed the APP component into a Class Component to set the token in STATE

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard'
@@ -6,23 +6,43 @@ import Preferences from './components/Preferences'
 import Login from './components/Login'
 
 
-function App() {
-  return (
-    <div className="container">
-      <h1 className="title">Hello from APP</h1>
-      <Login/>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard/>
-          </Route>
-          <Route path="/preferences">
-            <Preferences/>
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  )
+export class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      
+    }
+  }
+
+  setToken = (token) => {
+    this.setState({token: token});
+  }
+
+
+  render() { 
+
+    if (!this.state.token) {
+      return <Login setToken={this.setToken} />
+    }
+
+
+    return (
+      <div className="container">
+        <h1 className="title">Hello from APP</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard/>
+            </Route>
+            <Route path="/preferences">
+              <Preferences/>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,21 +3,26 @@ import React, { Component } from 'react'
 export class Login extends Component {
   render() {
     return (
-      <form>
-        <label htmlFor="">
-          <p>Username</p>
-          <input type="text"/>
-        </label>
-        <label htmlFor="">
-          <p>Password</p>
-          <input type="password"/>
-        </label>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+
+      <div className="login-form">
+        <h1>Please login</h1>
+        <form>
+          <label htmlFor="">
+            <p>Username</p>
+            <input type="text"/>
+          </label>
+          <label htmlFor="">
+            <p>Password</p>
+            <input type="password"/>
+          </label>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
     )
   }
 }
 
 export default Login
+
