9c75f79b1ca59c777903e554d76769de65a36c60
Tom <tomecrepont@gmail.com>
2021-04-14 19:41:29 +0200

    Set the open state

--- src/components/DropDownInput.js
+++ src/components/DropDownInput.js
@@ -5,17 +5,25 @@ export class DropDownInput extends Component {
     super(props)
   
     this.state = {
-       
+      open: false
     }
+  }
+
+  handleClick = () => {
+    console.log(this.props.name+" has been clicked")
+    this.setState({open: !this.state.open})
   }
   
 
   render() {
     return (
       <div className="drop-down-input">
-        <input type={this.props.type} name={this.props.name} />
+        <input type={this.props.type} name={this.props.name} onClick={this.handleClick}/>
         <div className="drop-down-propositions">
-          
+          <p>Proposition #1</p>
+          <p>Proposition #2</p>
+          <p>Proposition #3</p>
+          <p>Proposition #4</p>
         </div>
       </div>
     )
