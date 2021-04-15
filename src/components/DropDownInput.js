c25d0202ff509726a43a75bffb20488486e91a49
Tom <tomecrepont@gmail.com>
2021-04-15 09:25:01 +0200

    Set the handleClick to toggle the class Open wich togle the css display block/none

--- src/components/DropDownInput.js
+++ src/components/DropDownInput.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import "./DropDown.css"
 
 export class DropDownInput extends Component {
   constructor(props) {
@@ -16,10 +17,11 @@ export class DropDownInput extends Component {
   
 
   render() {
+    const dropDownPropositions = this.state.open ? "drop-down-propositions open" : "drop-down-propositions"
     return (
       <div className="drop-down-input">
         <input type={this.props.type} name={this.props.name} onClick={this.handleClick}/>
-        <div className="drop-down-propositions">
+        <div className={dropDownPropositions} >
           <p>Proposition #1</p>
           <p>Proposition #2</p>
           <p>Proposition #3</p>
