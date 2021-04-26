2279fe49c7647dd6191cb902fc098e7ddf88e4f2
Tom <tomecrepont@gmail.com>
2021-04-26 14:35:41 +0200

    Set the state one step higher, it is now ready to be used as a form input value for group creation

--- src/components/UserSearchEngine.js
+++ src/components/UserSearchEngine.js
@@ -7,6 +7,7 @@ export class UserSearchEngine extends Component {
     super(props)
   
     this.state = {
+      selectedUsers: []
        
     }
   }
@@ -15,15 +16,35 @@ export class UserSearchEngine extends Component {
     this.setState({hitsList: hits})
   }
 
-  setUsersSelected = (users) => {
-    this.setState({usersSelected: users})
+  setUserSelected = (newList) => {
+    this.setState({selectedUsers: newList})
   }
+
+  addSelectedUser = (user) => {
+    const selectedUsers = this.state.selectedUsers
+    const serializedUser=[user.id, user.last_name, user.first_name, user.photo_id]
+    selectedUsers.push(serializedUser)
+    this.setUserSelected(selectedUsers)
+  }
+
+  removeSelectedUser = (id) => {
+    console.log("Hello form remove user")
+    const actualUsersSelected = this.state.selectedUsers
+    const indexOfTheUser = actualUsersSelected.findIndex(element => element[0] === id)
+    actualUsersSelected.splice(indexOfTheUser,1)
+    this.setUserSelected(actualUsersSelected)
+  }
+
+
+
+
+
   
   render() {
     return (
       <div className="UserSearchEngine">
         <UserSearchEngineBar token={this.props.token} setHitsList={this.setHitsList}/>
-        <UserSearchEngineHits hitsList={this.state.hitsList} setUsersSelected={this.setUsersSelected}/>
+        <UserSearchEngineHits hitsList={this.state.hitsList} usersSelected={this.state.selectedUsers} addSelectedUser={this.addSelectedUser} removeSelectedUser={this.removeSelectedUser}    />
       </div>
     )
   }
