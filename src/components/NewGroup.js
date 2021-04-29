53e3c725f2498d9158537ba4afd59b35919bc411
Tom Ecrepont <tomecrepont@gmail.com>
2021-04-29 18:50:37 +0200

    creation of the #setInputValue method to set the state of the new group form with the input to send in the request

--- src/components/NewGroup.js
+++ src/components/NewGroup.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react'
 import UserSearchEngine from './UserSearchEngine'
 
 export class NewGroup extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+  }
+
+  setInputValue = (input) => {this.setState({inputValue: input})}
+
   handleSubmit = (event) => {
     event.preventDefault()
-    console.log(event)
+    const input = {name: event.target[0].value,
+                  users: this.state.inputValue.map(user => user[0])
+                  }
+    console.log(input)
   }
 
   render() {
@@ -16,7 +29,7 @@ export class NewGroup extends Component {
             <h1 className="NewGroup-label-name">Nom :</h1>
             <input type="text" name="name" id="NewGroup-input-name" className="NewGroup-input-name"/>
           </label>
-          <UserSearchEngine token={this.props.token}/>
+          <UserSearchEngine token={this.props.token} setInputValue={this.setInputValue}/>
           <input type="submit" value="Create new group"/>
 
         </form>
